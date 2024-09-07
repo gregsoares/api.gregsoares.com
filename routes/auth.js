5ae1e66605e51b0da1f8726eead68a39bfdd5258
Greg Soares <gregselvagem@gmail.com>
2024-09-07 17:52:19 -0400

    fix authController call

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-router.get('/', authController.handleAuth);
+router.get('/', authController.handleAuth(req, res));
 
 module.exports = router;
